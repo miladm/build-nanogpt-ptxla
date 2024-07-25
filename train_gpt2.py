9e48bcd4447fc058a668aa66a7c06803cab0da5d
Milad Mohammadi <milad.mo@gmail.com>
2024-07-25 06:29:53 +0000

    adding set_float32_matmul_precision and timer

--- train_gpt2.py
+++ train_gpt2.py
@@ -1,4 +1,5 @@
 import math
+import time
 from dataclasses import dataclass
 import torch
 import torch_xla
@@ -221,7 +222,9 @@ def train_gpt():
     if torch.cuda.is_available():
         torch.cuda.manual_seed(1337)
 
-    train_loader = DataLoaderLite(B=4, T=32)
+    train_loader = DataLoaderLite(B=16, T=1024)
+
+    torch.set_float32_matmul_precision('high')
 
     # get logits
     model = GPT(GPTConfig())
@@ -230,17 +233,29 @@ def train_gpt():
     # optimize!
     optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4)
     for i in range(50):
+        t0 = time.time()
         x, y = train_loader.next_batch()
         x, y = x.to(device), y.to(device)
         optimizer.zero_grad()
         logits, loss = model(x, y)
         loss.backward()
-        xm.optimizer_step(optimizer) #TORCHXLA replaces: optimizer.step()
         print(f"step {i}, loss: {loss.item()}")
+        optimizer.step()
+        if is_tpu_available():
+            xm.mark_step()
+        else:
+            torch.cuda.synchronize() # wait for the GPU to finish work
+        t1 = time.time()
+        dt = (t1 - t0)*1000 # time difference in miliseconds
+        tokens_per_sec = (train_loader.B * train_loader.T) / (t1 - t0)
+        print(f"step {i}, loss: {loss.item()}, dt: {dt:.2f}ms, tok/sec: {tokens_per_sec:.2f}")
 
 # Check if TPU is available
 def is_tpu_available():
-    return xm.xrt_world_size() > 1
+    devices = xm.xla_real_devices()
+    if len(devices) > 0:
+        return 'TPU' in devices[0]
+    return False
 
 def _mp_fn(index, flags=None):
   train_gpt()
